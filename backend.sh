d73b2e779fc138081b2e46eda1f757300fdc3efc
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2024-05-06 11:31:23 +0100

    new fun commit

--- backend.sh
+++ backend.sh
@@ -12,57 +12,52 @@ read -s mysql_root_password
 
 
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs:20 version"
+
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodeJs"
+
 
 id expense  &>>$LOGFILE                     
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE                            # useradd expense command is not idempotency so we are changing the code
-    VALIDATE $? "creating expense user"
 else   
     echo -e "Expense user already created..... $Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE 
-VALIDATE $? "creating app directory"
+
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE 
-VALIDATE $? "downloading backend code"
 
 cd /app
 
 rm -rf /app/*                 #everytime if you unzip a folder which is already unziped, it will throw a error. so before unziping remove all the file in that directory and then unzip it 
 
 unzip /tmp/backend.zip &>>$LOGFILE 
-VALIDATE $? "extracted backend code"
+
 
 npm install &>>$LOGFILE 
-VALIDATE $? "Installing nodeJS dependencies"
+
 
 cp /home/ec2-user/expense-shellscript/backend.service /etc/systemd/system/backend.service &>>$LOGFILE 
-VALIDATE $? "copied backend service"
+
 
 
 systemctl daemon-reload &>>$LOGFILE 
-VALIDATE $? "Daemon Reload"
+
 
 systemctl start backend &>>$LOGFILE 
-VALIDATE $? "starting backend"
 
 systemctl enable backend &>>$LOGFILE 
-VALIDATE $? "Enabling Backend"
+
 
 dnf install mysql -y &>>$LOGFILE 
-VALIDATE $? "installing MYSQL client"
+
 
 mysql -h mysql.devopsnavyahome.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE   # mysql.devopsnavyahome.online---mysql BD server ip ----configure in R53, A--record
-VALIDATE $? "schema loading"
+
 
 systemctl restart backend &>>$LOGFILE 
-VALIDATE $? "Restarting backend"
